91eb441db236800cae71cd5629f3cf40ba4b9aea
Glen Jarvis <glen@glenjarvis.com>
2026-01-07 17:38:00 -0800

    Refactor to generic useData hook

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -3,19 +3,14 @@ import { CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 
-interface Genre {
-  id: number;
-  name: string;
-}
 
-interface FetchGenresResponse {
+interface FetchResponse<T> {
   count: number;
-  results: Genre[];
-
+  results: T[];
 }
 
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
@@ -24,9 +19,9 @@ const useGenres = () => {
 
     setLoading(true);
     apiClient
-      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
       .then((res) => {
-        setGenres(res.data.results);
+        setData(res.data.results);
         setLoading(false);
       })
       .catch((err) => {
@@ -37,7 +32,7 @@ const useGenres = () => {
 
     return () => controller.abort();
   }, []);
-  return { genres, error, isLoading };
+  return { data, error, isLoading };
 }
 
-export default useGenres
+export default useData
