e312589c3de8c092cd9fa21ea09877fe7b2761be
Glen Jarvis <glen@glenjarvis.com>
2026-01-14 13:16:39 -0800

    Convert theme from Chakra v2 to v3

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, defaultConfig, createSystem } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes";
 
 import App from "./App";
 import "./index.css";
-
-const system = createSystem(defaultConfig);
+import { system } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="dark">
       <ChakraProvider value={system}>
         <App />
       </ChakraProvider>
