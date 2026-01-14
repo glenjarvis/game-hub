cbd657fdf3890dbc7c37c7116701cec2cbf319eb
Glen Jarvis <glen@glenjarvis.com>
2026-01-13 18:49:19 -0800

    Add next-themes for Dark Mode management
    Unlike Chakra v2, Chakra v3 wants us to manage themes externally to Chakra. A nice tool to do that is `next-themes`. Install and implement it in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, defaultConfig, createSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
 
 import App from "./App";
 import "./index.css";
@@ -9,8 +10,10 @@ const system = createSystem(defaultConfig);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value={system}>
-      <App />
-    </ChakraProvider>
+    <ThemeProvider attribute="class">
+      <ChakraProvider value={system}>
+        <App />
+      </ChakraProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
