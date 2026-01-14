f3a01ee4df0937649824cb68a4945439e5c8d92b
Glen Jarvis <glen@glenjarvis.com>
2026-01-13 18:43:05 -0800

    BREAKING CHANGE: Start migrating main.tsx to Chakra3

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// CHAKRA_V3_FIX: Commented out all Chakra imports
-// import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider, defaultConfig, createSystem } from "@chakra-ui/react";
 
 import App from "./App";
-// CHAKRA_V3_FIX: Commented out theme import
-// import theme from "./theme";
 import "./index.css";
+
+const system = createSystem(defaultConfig);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* CHAKRA_V3_FIX: All Chakra providers commented out below */}
-    {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
-    {/* <ChakraProvider theme={theme}> */}
-    {/*   <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
-    <App />
-    {/* </ChakraProvider> */}
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
